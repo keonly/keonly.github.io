50e879a915d47fafcc5f247c56eda889234d0b8c
keonly <keonl@kaist.ac.kr>
2024-01-14 04:13:44 +0900

    fix: giscus dynamic theming

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -1,28 +1,15 @@
+import commentsScript from "./scripts/comments.inline"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 export default (() => {
-  function Comments({ cfg, fileData }: QuartzComponentProps) {
+  function Comments({ cfg, fileData, theme }: QuartzComponentProps) {
     if (fileData.frontmatter.comments === false) return null
     else
       return (
-        <script
-          src="https://giscus.app/client.js"
-          data-repo="keonly/keonly.github.io"
-          data-repo-id="R_kgDOKwHh3A"
-          data-category="Comments"
-          data-category-id="DIC_kwDOKwHh3M4CbHnd"
-          data-mapping="og:title"
-          data-strict="0"
-          data-reactions-enabled="1"
-          data-emit-metadata="0"
-          data-input-position="bottom"
-          data-theme="light"
-          data-lang="en"
-          crossorigin="anonymous"
-          async
-        ></script>
+        <div></div>
       )
   }
 
+  Comments.afterDOMLoaded = commentsScript
   return Comments
 }) satisfies QuartzComponentConstructor
---
+++ quartz/components/scripts/comments.inline.ts
@@ -0,0 +1,28 @@
+function getGiscusTheme() {
+  const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
+  const currentTheme = localStorage.getItem("theme") ?? userPref
+  return currentTheme
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "keonly/keonly.github.io",
+    "data-repo-id": "R_kgDOKwHh3A",
+    "data-category": "Comments",
+    "data-category-id": "DIC_kwDOKwHh3M4CbHnd",
+    "data-mapping": "og:title",
+    "data-strict": "0",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "top",
+    "data-theme": getGiscusTheme(),
+    "data-lang": "en",
+    crossorigin: "anonymous",
+    async: "",
+  }
+
+  const giscusScript = document.createElement("script")
+  Object.entries(giscusAttributes).forEach(([key, value]) => giscusScript.setAttribute(key, value))
+  document.body.appendChild(giscusScript)
+})
